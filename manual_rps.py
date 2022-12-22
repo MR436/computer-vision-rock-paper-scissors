51375ecb2a8e00dca2171379f7c4386289e9f0f0
MR436 <mandeepr78@gmail.com>
2022-12-22 11:52:58 +0000

    task 2 completed

--- manual_rps.py
+++ manual_rps.py
@@ -7,11 +7,25 @@ def get_computer_choice():
     print(f"The computer chose:{computer_choice}")
 
 def get_user_choice():
-    user_choice = input("Please enter your choice")
+    user_choice = input("Please enter your choice: ")
     print(f"{user_choice}")
+
+def get_winner(computer_choice, user_choice):
+    if computer_choice == "rock" and user_choice == "paper":
+        return ("You won") 
+    elif computer_choice == "paper" and user_choice == "scissor":
+        return("You Won")
+    elif computer_choice == "scissor" and user_choice == "rock":
+        return("You Won")
+    elif computer_choice == user_choice:
+        return("Tie")
+    else:
+        return("You lost")
+
 
 get_computer_choice()
 get_user_choice()
+get_winner()
 
 
 
