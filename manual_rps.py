de2d813b4f23fcbc56e1cff5d86b4dfad30fbe3c
MR436 <mandeepr78@gmail.com>
2022-12-22 13:55:50 +0000

    task 1 completed

--- manual_rps.py
+++ manual_rps.py
@@ -1,31 +1,33 @@
 import random
 from random import choice
-list = ["rock", "paper", "scissors"]
+choices = ["rock", "paper", "scissors"]
 
 def get_computer_choice():
-    computer_choice = random.choice(list)
+    computer_choice = random.choice(choices)
     print(f"The computer chose:{computer_choice}")
+    return computer_choice
 
 def get_user_choice():
     user_choice = input("Please enter your choice: ")
     print(f"{user_choice}")
+    return user_choice
 
-def get_winner(computer_choice, user_choice):
-    if computer_choice == "rock" and user_choice == "paper":
-        return ("You won") 
-    elif computer_choice == "paper" and user_choice == "scissor":
-        return("You Won")
-    elif computer_choice == "scissor" and user_choice == "rock":
-        return("You Won")
-    elif computer_choice == user_choice:
-        return("Tie")
-    else:
-        return("You lost")
+# def get_winner(computer_choice, user_choice):
+#     if computer_choice == "rock" and user_choice == "paper":
+#         return ("You won") 
+#     elif computer_choice == "paper" and user_choice == "scissor":
+#         return("You Won")
+#     elif computer_choice == "scissor" and user_choice == "rock":
+#         return("You Won")
+#     elif computer_choice == user_choice:
+#         return("Tie")
+#     else:
+#         return("You lost")
 
 
 get_computer_choice()
 get_user_choice()
-get_winner()
+#get_winner()
 
 
 
