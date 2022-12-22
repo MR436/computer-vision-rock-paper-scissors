a373bfdc8cb11397c2022ad530845bf3e4369eae
MR436 <mandeepr78@gmail.com>
2022-12-22 14:57:19 +0000

    task 2 completed

--- manual_rps.py
+++ manual_rps.py
@@ -15,20 +15,19 @@ def get_user_choice():
 
 
 def get_winner(computer_choice, user_choice):
-    if computer_choice == "rock" and user_choice == "paper":
-        return ("You won") 
-    elif computer_choice == "paper" and user_choice == "scissor":
-        return("You Won")
-    elif computer_choice == "scissor" and user_choice == "rock":
-        return("You Won")
-    elif computer_choice == user_choice:
-        return("Tie")
+    if computer_choice == user_choice:
+        print("It is a tie!")
+    elif (computer_choice == "Rock" and user_choice == "Paper") or \
+         (computer_choice == "Paper" and user_choice == "Scissor") or \
+         (computer_choice == "Scissor" and user_choice == "Rock"):
+        #return user_choice
+        print("You Won!")
     else:
-        return("You lost")
+        print("You lost")
 
-
-get_computer_choice()
-get_user_choice()
+user_choice = get_user_choice()
+computer_choice = get_computer_choice()
+get_winner(computer_choice, user_choice)
 
 
 
